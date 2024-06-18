7088450abdc7a2c72163a697a4c3f419b94f9e5b
JosePereiraF <jose222125santos@gmail.com>
2024-06-18 14:21:25 -0300

    add cardsHome funcionando

--- Atividade-01-ReactNative/components/CardProduto/index.tsx
+++ Atividade-01-ReactNative/components/CardProduto/index.tsx
@@ -1,20 +1,37 @@
-import { View, Text,Image } from 'react-native'
+import { View, Text,Image,StyleSheet } from 'react-native'
 import React from 'react'
 
+interface CardProdutoProps{
+  imagem:string,
+  valor:number,
+  nome:string
+}
 
-export default function CardProduto(imagem:string,valor:number,nome:string ) {
+export default function CardProduto({imagem,valor,nome}:CardProdutoProps ) {
+ 
   return (
-    <View>
+    <View style={styles.container}>
       <Image 
-      source={require(imagem)}
-      style={{ width: 65, height: 65 }}
+      source={imagem=="imagem1"?require("../../assets/image-removebg-preview.png"):imagem=="imagem2"?require("../../assets/image-removebg-preview (1).png"):imagem=="imagem3"?require("../../assets/image-removebg-preview (2).png"):require("../../assets/image-removebg-preview (3).png")}
+      style={{ width: 65, height: 85 }}
       />
         <Text>
         {nome}
         </Text>
         <Text>
-            {valor}
+            R${valor}.00
         </Text>
     </View>
   )
 }
+const styles = StyleSheet.create({
+  container: {
+    height: 150,
+    width: 100,
+    backgroundColor: 'lightgray',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    borderRadius: 8, 
+  },
+});
--- Atividade-01-ReactNative/screens/Home/index.tsx
+++ Atividade-01-ReactNative/screens/Home/index.tsx
@@ -1,32 +1,41 @@
 import { View, Text ,StyleSheet} from 'react-native'
 import React, { useState } from 'react'
-import { FlatList } from 'react-native-gesture-handler';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 import CardProduto from '../../components/CardProduto';
 
 export default function Home() {
 
   const[produtos,setProdutos] = useState([
-    { id:1, nome: "Jorge", valor:71,image:"../../assets/image-removebg-preview.png" },
-    { id:2, nome: "Jonas", valor:24,image:"image-removebg-preview (1).png" },
-    { id:3, nome: "Georg", valor:47,image:"image-removebg-preview (2).png" },
-    { id:4, nome: "Ricks", valor:21,image:"image-removebg-preview (3).png" },
+    { id:1, nome: "Randall Park", valor:71,image:"imagem1" },
+    { id:2, nome: "Elijah Wood", valor:24,image:"imagem2" },
+    { id:3, nome: "Mario", valor:47,image:"imagem3" },
+    { id:4, nome: "Yvette Nicole ", valor:47,image:"imagem4" },
+    
     ]);
   return (
-    <View>
-        <FlatList
-data={produtos}
-renderItem={({ item })=>{
-  return <CardProduto />
-}}
-/>
+    <ScrollView
+    >
+    <View style={styles.container}>
+      <FlatList
+      data={produtos}
+      keyExtractor={(item) => item.id.toString()}
+      renderItem={({item})=>{
+        return <CardProduto
+        valor={item.valor}
+        nome={item.nome}
+        imagem={item.image}
+        />
+      }}
+      />
     </View>
+    </ScrollView>
   )
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingVertical: 20,
+    alignItems:"center"
   },
 });

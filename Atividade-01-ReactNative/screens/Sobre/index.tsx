96be0283e1b06093aedf64c5b3466f0a550670a2
Ianzinzin <162218082+Ianzinzin@users.noreply.github.com>
2024-06-18 15:40:53 -0300

    sobre.

--- Atividade-01-ReactNative/screens/Sobre/index.tsx
+++ Atividade-01-ReactNative/screens/Sobre/index.tsx
@@ -1,20 +1,49 @@
-import { View, Text ,StyleSheet} from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
 
 export default function Sobre() {
-  //colocar um arrray para controlar as informações a serem mostradas ou usar um TEXT
-
   return (
-    <View>
-      <Text> Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis, quam. Harum sed porro sint similique voluptas autem expedita aliquam! Ipsa eius odit, fugiat voluptates sed deleniti accusamus! Vitae, numquam neque!</Text>
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.title}>Sobre Nós</Text>
+        <Text style={styles.description}>
+          Bem-vindo à nossa loja de pelúcias! Somos apaixonados por trazer alegria e conforto através de nossas pelúcias de alta qualidade. Cada um de nossos produtos é escolhido com carinho para garantir que você encontre o amigo perfeito para abraçar e amar.
+        </Text>
+        <Text style={styles.description}>
+          Nossa missão é proporcionar momentos de felicidade e ternura, seja para presentear alguém especial ou para adicionar um toque de fofura ao seu dia. Acreditamos que pelúcias são mais do que simples brinquedos, são companheiros que fazem parte de nossas vidas.
+        </Text>
+        <Text style={styles.description}>
+          Obrigado por visitar nossa loja. Esperamos que você encontre exatamente o que procura e que nossas pelúcias tragam um sorriso ao seu rosto!
+        </Text>
+      </View>
     </View>
-  )
+  );
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5f5',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  textContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 0.8)', // Cor branca com 80% de opacidade
+    padding: 20,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333',
+    textAlign: 'center',
+  },
+  description: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 15,
+    color: '#666',
   },
 });

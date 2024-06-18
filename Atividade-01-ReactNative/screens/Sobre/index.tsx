463e215ca4f8bfbd9a4557ef599fe7f586fbe3e6
JosePereiraF <jose222125santos@gmail.com>
2024-06-17 21:07:02 -0300

    first commit

---
+++ Atividade-01-ReactNative/screens/Sobre/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Sobre() {
+  //colocar um arrray para controlar as informações a serem mostradas ou usar um TEXT
+
+  return (
+    <View>
+      <Text>Sobre</Text>
+    </View>
+  )
+}
